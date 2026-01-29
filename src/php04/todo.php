43ddf3110db1bd03ee26ac94dc4da2b9c55d4b39
kumiko <crumin001@gmail.com>
2026-01-30 01:47:41 +0900

    ToDoに×削除ボタンを追加

--- src/php04/todo.php
+++ src/php04/todo.php
@@ -26,18 +26,41 @@ $todos = $_SESSION['todos'];
 <head>
   <meta charset="UTF-8">
   <title>ToDoアプリ</title>
+  <style>
+  .delete-btn {
+    background: none;
+    border: none;
+    color: red;
+    font-size: 18px;
+    cursor: pointer;
+    margin-left: 8px;
+  }
+
+  .delete-btn:hover {
+    opacity: 0.7;
+  }
+</style>
+
 </head>
 <body>
 
 <h1>ToDoアプリ</h1>
-
+<h4>完了したら×を押してください</h4>
 <form method="post">
   <input type="text" name="todo" placeholder="やることを入力">
   <button type="submit">追加</button>
 </form>
 <ul>
-  <?php foreach ($todos as $todo): ?>
-    <li><?php echo htmlspecialchars($todo, ENT_QUOTES); ?></li>
+   <?php foreach ($todos as $index => $todo): ?>
+    <li>
+      <?php echo htmlspecialchars($todo, ENT_QUOTES); ?>
+
+      <form method="post" style="display:inline">
+        <button type="submit" name="delete" value="<?php echo $index; ?>" class="delete-btn">
+          ×
+        </button>
+      </form>
+    </li>
   <?php endforeach; ?>
   </ul>
   
